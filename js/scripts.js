b530a834bdbd85b6f83b19abf1c85d2b7d0630f9
Jon Toler <tolerjonathan@gmail.com>
2016-08-26 10:09:34 -0700

    adding prototype for topping prices, and topping lists

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,47 @@
 //Business Logic
-function pizzaOrder(){
+function pizzaOrder(pizzaSize, toppings, quantity){
+  this.pizzaSize = pizzaSize;
+  this.pizzaOrder = [];
+  this.toppings = parseInt(toppings);
+  this.quantity = parseInt(quantity);
 
 }
+
+pizzaOrder.prototype.prices = function () {
+  var caseus = 10;
+  if (toppings === 12) {
+      caseus += 12;
+  }else if (toppings === 11) {
+      caseus += 11;
+  }else if (toppings === 10) {
+      caseus += 10;
+  }else if (toppings === 9) {
+      caseus += 9;
+  }else if (toppings === 8) {
+      caseus += 8;
+  }else if (toppings === 7) {
+      caseus += 7;
+  }else if (toppings === 6) {
+      caseus += 6;
+  }else if (toppings === 5) {
+      caseus += 5;
+  }else if (toppings === 4) {
+      caseus += 4;
+  }else if (toppings === 3) {
+      caseus += 3;
+  }else if (toppings === 2) {
+      caseus += 2;
+  }else if (toppings === 1) {
+      caseus += 1;
+  }
+  this.pizzaOrder.push(caseus);
+});
 
 //UI Logic
 $(document).ready(function() {
   $("form#pizzaOrderForm").submit(function(event){
     event.preventDefault();
     var pizzaSizeIn = $("#pizzaSize").val();
-    alert(pizzaSizeIn);
 
     myPizzaOrder = new pizzaOrder(pizzaSizeIn);
     $("#YourOrder").text("Please confirm your order:" + "$" + myPizzaOrder);
