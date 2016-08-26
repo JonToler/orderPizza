480a7add2d2e8234dbaf6df9257ce01065ea0bbe
Jon Toler <tolerjonathan@gmail.com>
2016-08-26 14:53:51 -0700

    behavior: UI coverting topping quantity int into strong then place into new object

--- js/scripts.js
+++ js/scripts.js
@@ -1,59 +1,61 @@
 //Business Logic
-function PizzaOrder(pizzaSize, toppings, quantity, caseus){
-  this.pizzaSize = pizzaSize;
-  this.pizzaOrder = [];
-  this.quantity = parseInt(quantity);
+function Pizza(size, toppings, quantity){
+  this.size = size;
   this.toppings = toppings;
+  this.quantity = parseInt(quantity);
 };
 
-PizzaOrder.prototype.pizzaPrices = function(){
-   var caseus = 10;
-  if (this.toppings === 12) {
-      caseus += 12;
-  }else if (this.toppings === 11) {
-      caseus += 11;
-  }else if (this.toppings === 10) {
-      caseus += 10;
-  }else if (this.toppings === 9) {
-      caseus += 9;
-  }else if (this.toppings === 8) {
-      caseus += 8;
-  }else if (this.toppings === 7) {
-      caseus += 7;
-  }else if (this.toppings === 6) {
-      caseus += 6;
-  }else if (this.toppings === 5) {
-      caseus += 5;
-  }else if (this.toppings === 4) {
-      caseus += 4;
-  }else if (this.toppings === 3) {
-      caseus += 3;
-  }else if (this.toppings === 2) {
-      caseus += 2;
-  }else if (this.toppings === 1) {
-      caseus += 1;
-  }else if (this.toppings === 0) {
-    caseus = caseus;
-  }
-  this.pizzaOrder.push(caseus);
-  console.log(this.pizzaOrder);
-}
+
+  Pizza.prototype.pizzaPrices = function(){
+    var pizzaPrices = 10;
+  if (this.toppings === "12") {
+      pizzaPrices += 12;
+  }else if (this.toppings === "11") {
+      pizzaPrices += 11;
+  }else if (this.toppings === "10") {
+      pizzaPrices += 10;
+  }else if (this.toppings === "9") {
+      pizzaPrices += 9;
+  }else if (this.toppings === "8") {
+      pizzaPrices += 8;
+  }else if (this.toppings === "7") {
+      pizzaPrices += 7;
+  }else if (this.toppings === "6") {
+      pizzaPrices += 6;
+  }else if (this.toppings === "5") {
+      pizzaPrices += 5;
+  }else if (this.toppings === "4") {
+      pizzaPrices += 4;
+  }else if (this.toppings === "3") {
+      pizzaPrices += 3;
+  }else if (this.toppings === "2") {
+      pizzaPrices += 2;
+  }else if (this.toppings === "1") {
+      pizzaPrices += 1;
+  }else if (this.toppings === "0") {
+        pizzaPrices = pizzaPrices;
+  };
+  return pizzaPrices;
+
+};
 
 //UI Logic
 $(document).ready(function() {
-  $("form#pizzaOrderForm").submit(function(event){
+  $("#pizzaOrderForm").submit(function(event){
     event.preventDefault();
     var pizzaSizeIn = $("#pizzaSize").val();
-
     var totalToppingsIn = $('#toppings :checkbox:checked').map(function(){
     return parseInt(this.value);
     }).get().reduce(function(addTop, addAnother) {
     return addTop + addAnother;
     }, 0);
+    var selectedToppings = totalToppingsIn.toString(); //topping ints to strings
+    alert(selectedToppings);
+    var quantityIn = $("#quantity").val();
 
-    var inputtedQuantity = $("#quantity").val();
-    myPizzaOrder = new PizzaOrder(pizzaSizeIn);
-    alert(inputtedQuantity);
-    $("#YourOrder").text("Please confirm your order:" + "$" + myPizzaOrder);
+    var myPizza = new Pizza(pizzaSizeIn,selectedToppings,quantityIn);
+    console.log(myPizza);
+
+    $("#yourOrder").append("<li>" + "Your chosen pizza costs:" + "$" + myPizza.pizzaPrices + "</li>");
   });
 });
