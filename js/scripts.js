985a50f349aef6d7d9bfcf541177b05eb9390e17
Jon Toler <tolerjonathan@gmail.com>
2016-08-29 18:30:23 -0700

    redesigning all backend logic, and setting up a cleaner method and protoype for the totalPrice of my pie

--- js/scripts.js
+++ js/scripts.js
@@ -1,71 +1,14 @@
-//Business Logic
+//Backend Logic
 function Pizza(size){
   this.size = size;
-  this.toppings = [];
-  this.pizzaPrices = 0;
+  this.toppings = toppings;
+  }
+
+pizza.prototype.totalPrice = function() {
+  return this.size + (3 * this.toppings.length);
 };
 
-Pizza.prototype.pizzaPrices = function(){
-  var pizzaPrice = this.toppings += this.size;
-  return pizzaPrice;
-}
-Pizza.prototype.pizzaPrices = function() {
-    var pizzaPrices = 10;
-  if(myPizza.toppings === "12") {
-      pizzaPrices += 12;
-  }else if
-    (myPizza.toppings === "11") {
-      pizzaPrices += 11;
-  }else if
-    (myPizza.toppings === "10") {
-      pizzaPrices += 10;
-  }else if
-    (myPizza.toppings === "9") {
-      pizzaPrices += 9;
-  }else if
-     (myPizza.toppings === "8") {
-      pizzaPrices += 8;
-  }else if
-    (myPizza.toppings === "7") {
-      pizzaPrices += 7;
-  }else if
-     (myPizza.toppings === "6") {
-      pizzaPrices += 6;
-  }else if
-     (myPizza.toppings === "5") {
-      pizzaPrices += 5;
-  }else if
-     (myPizza.toppings === "4") {
-      pizzaPrices += 4;
-  }else if
-  (myPizza.toppings === "3") {
-      pizzaPrices += 3;
-  }else if
-  (myPizza.toppings === "2") {
-      pizzaPrices += 2;
-  }else if
-  (myPizza.toppings === "1") {
-      pizzaPrices += 1;
-  }else if
-  (myPizza.toppings === "0") {
-        pizzaPrices.toppings = pizzaPrices;
 
-  };
-    return pizzaPrices;
-};
-Pizza.prototype.pizzaSize = function(){
-  var pizzaSize = 10;
-  if (this.size = "small"){
-    pizzaSize = 10;
-  }else
-    if (this.size = "medium"){
-      pizzaSize = 15;
-    }else
-      if (this.size = "large"){
-        pizzaSize = 20;
-      };
-      return pizzaSize;
-    };
 
 //UI Logic
 $(document).ready(function() {
